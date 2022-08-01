84a93b2ae76a5093270abed7da4cdb659f4a4523
rishikesh.banore <rishikesh.banore@wikifolio.com>
2022-08-01 23:32:18 +0200

    Added Programm for Date Object

---
+++ JS_DateObject.js
@@ -0,0 +1,18 @@
+//This Programm will help to understand date objects in Javascript.
+
+let date=new Date();
+
+console.log(date);
+
+console.log(date.getDate()); //Print Date from 1 to 31
+
+console.log(date.getMonth()); //Print Month from 0 to 11
+
+console.log(date.getMonth()+1); //Print Month from 1 to 12 (Exact Month)
+
+console.log(date.getFullYear()); //Print current year.
+
+console.log(d.getHours());    // 0-23
+console.log(d.getMinutes());  // 0-59
+console.log(d.getSeconds());  // 0-59
+
